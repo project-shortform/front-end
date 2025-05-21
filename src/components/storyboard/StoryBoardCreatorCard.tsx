208486cb08f090f19bef2bb9f2b23defd6c9785b
유림 <yulimh001@gmail.com>
2025-05-21 13:38:10 +0900

    feat: 넘어갈 경우 스크롤

--- src/components/storyboard/StoryBoardCreatorCard.tsx
+++ src/components/storyboard/StoryBoardCreatorCard.tsx
@@ -4,9 +4,8 @@ interface StoryBoardCreatorCardProps {
 const StoryBoardCreatorCard = (props: StoryBoardCreatorCardProps) => {
   const { children } = props
   return (
-    //
     <div className="border-gray-5 bg-color relative flex w-[76.188rem] flex-col rounded-[2.5rem] border p-[2.5rem] xl:h-[600px] 2xl:h-[700px]">
-      <div className="flex h-full flex-col justify-between gap-y-[2.5rem] 2xl:gap-y-[20px]">{children}</div>
+      <div className="flex flex-col justify-between gap-y-[2.5rem] overflow-y-scroll 2xl:gap-y-[20px]">{children}</div>
     </div>
   )
 }
