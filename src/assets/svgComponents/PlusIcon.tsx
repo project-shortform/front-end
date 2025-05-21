202bfd94f963366537c7d4739c7f00ee1509b4b8
유림 <yulimh001@gmail.com>
2025-05-21 13:07:50 +0900

    feat: icon 에러 수정

--- src/assets/svgComponents/PlusIcon.tsx
+++ src/assets/svgComponents/PlusIcon.tsx
@@ -1,6 +1,25 @@
-import * as React from "react";
-import type { SVGProps } from "react";
-const SvgPlusIcon = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}><mask id="plus-icon_svg__a" width={24} height={24} x={0} y={0} maskUnits="userSpaceOnUse" style={{
-    maskType: "alpha"
-  }}><path fill="#D9D9D9" d="M0 0h24v24H0z" /></mask><g mask="url(#plus-icon_svg__a)"><path fill="#F7F7F7" d="M11.25 12.75h-5a.73.73 0 0 1-.534-.216A.73.73 0 0 1 5.5 12q0-.32.216-.534a.73.73 0 0 1 .534-.216h5v-5q0-.319.216-.534A.73.73 0 0 1 12 5.5q.32 0 .534.216a.73.73 0 0 1 .216.534v5h5q.318 0 .534.216A.73.73 0 0 1 18.5 12q0 .32-.216.534a.73.73 0 0 1-.534.216h-5v5q0 .318-.216.534A.73.73 0 0 1 12 18.5a.73.73 0 0 1-.534-.216.73.73 0 0 1-.216-.534z" /></g></svg>;
-export default SvgPlusIcon;
+import type { SVGProps } from 'react'
+const SvgPlusIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <mask
+      id="plus-icon_svg__a"
+      width={24}
+      height={24}
+      x={0}
+      y={0}
+      maskUnits="userSpaceOnUse"
+      style={{
+        maskType: 'alpha',
+      }}
+    >
+      <path fill="#D9D9D9" d="M0 0h24v24H0z" />
+    </mask>
+    <g mask="url(#plus-icon_svg__a)">
+      <path
+        fill="#F7F7F7"
+        d="M11.25 12.75h-5a.73.73 0 0 1-.534-.216A.73.73 0 0 1 5.5 12q0-.32.216-.534a.73.73 0 0 1 .534-.216h5v-5q0-.319.216-.534A.73.73 0 0 1 12 5.5q.32 0 .534.216a.73.73 0 0 1 .216.534v5h5q.318 0 .534.216A.73.73 0 0 1 18.5 12q0 .32-.216.534a.73.73 0 0 1-.534.216h-5v5q0 .318-.216.534A.73.73 0 0 1 12 18.5a.73.73 0 0 1-.534-.216.73.73 0 0 1-.216-.534z"
+      />
+    </g>
+  </svg>
+)
+export default SvgPlusIcon
