4c026794a18ee86e62fdcfa46d13c893a3e4dbd7
유림 <yulimh001@gmail.com>
2025-05-21 12:04:32 +0900

    feat: 영상 생성 퍼블리싱

--- src/pages/MovieCreator.tsx
+++ src/pages/MovieCreator.tsx
@@ -2,12 +2,15 @@ import Header from '../components/common/Header.tsx'
 import { useState } from 'react'
 import Modal from '../components/common/Modal.tsx'
 import { GoogleLogo, KakaoLogo } from '../assets/svgComponents'
+import { useNavigate } from 'react-router-dom'
 
 const MovieCreator = () => {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false)
 
+  const navigate = useNavigate()
+
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-center">
       {isLoginModalOpen && (
         <Modal onClick={() => setIsLoginModalOpen(false)}>
           <section className="mt-[5rem] flex flex-col items-center justify-center gap-y-[6.25rem]">
@@ -26,6 +29,25 @@ const MovieCreator = () => {
         </Modal>
       )}
       <Header headerType={'DEFAULT'} onClick={() => setIsLoginModalOpen(true)} />
+      <div className="h-[80px]" />
+      <div className="relative flex flex-col items-center justify-center">
+        <section className="bg-color border-gray-5 border-gray-5 flex h-[736px] w-[1262px] flex-col items-center gap-y-5 rounded-[20px] border p-5">
+          <div className="mt-5 w-[800px]">
+            <div className="bg-gray-5 h-[474px]" />
+            <div className="mt-5 flex gap-x-2">
+              <input className="default-input w-full" placeholder={'영상의 제목을 입력해주세요.'}></input>
+              <button className="outline-default-button h-[48px] w-[120px]">저장</button>
+            </div>
+          </div>
+
+          <div className="absolute right-5 bottom-5 left-5 flex gap-x-6">
+            <button onClick={() => navigate('/edit-movie')} className="secondary-active-button h-[56px] w-full">
+              편집으로 돌아가기
+            </button>
+            <button className="active-button h-[56px] w-full">다운로드</button>
+          </div>
+        </section>
+      </div>
     </main>
   )
 }
