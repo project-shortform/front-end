d9656cb9270d7ca17da0af2c413e77a62ae6ad35
유림 <yulimh001@gmail.com>
2025-06-10 15:11:04 +0900

    feat: 최종 영상 결과 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Login from './pages/Login.tsx'
 import StoryBoard from './pages/StoryBoard.tsx'
 import MovieEditor from './pages/MovieEditor.tsx'
 import MovieCreator from './pages/MovieCreator.tsx'
+import Result from './pages/Result.tsx'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/storyboard" element={<StoryBoard />} />
           <Route path="/edit-movie" element={<MovieEditor />} />
           <Route path="/create-movie" element={<MovieCreator />} />
+          <Route path="/result" element={<Result />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Result.tsx
@@ -0,0 +1,41 @@
+import Header from '../components/common/Header.tsx'
+
+const Result = () => {
+  return (
+    <div>
+      <Header headerType={'DEFAULT'} />
+      <div className="grid-cols mt-[120px] grid w-full gap-6 px-[40px] lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3">
+        <section className="flex flex-col gap-y-2">
+          <h1 className="title-lg">온열처리 예방법</h1>
+          <video controls src={'http://49.143.34.88:5000/output/final_edit_55.mp4'} width={500} height={200} />
+        </section>
+
+        <section className="flex flex-col gap-y-2">
+          <h1 className="title-lg">성별에 따른 해킹</h1>
+          <video controls src={'http://49.143.34.88:5000/output/final_edit_53.mp4'} width={500} height={200} />
+        </section>
+
+        <section className="flex flex-col gap-y-2">
+          <h1 className="title-lg">챗GPT 열풍 감소</h1>
+          <video controls src={'http://49.143.34.88:5000/output/final_edit_56.mp4'} width={500} height={200} />
+        </section>
+
+        <section className="flex flex-col gap-y-2">
+          <h1 className="title-lg">주 4.5일제 찬반 의견</h1>
+          <video controls src={'http://49.143.34.88:5000/output/final_edit_57.mp4'} width={500} height={200} />
+        </section>
+
+        <section className="flex flex-col gap-y-2">
+          <h1 className="title-lg">관세</h1>
+          <video controls src={'http://49.143.34.88:5000/output/final_edit_47.mp4'} width={500} height={200} />
+        </section>
+
+        <section className="flex flex-col gap-y-2">
+          <h1 className="title-lg">지하철 시간 단축</h1>
+          <video controls src={'http://49.143.34.88:5000/output/final_edit_49.mp4'} width={500} height={200} />
+        </section>
+      </div>
+    </div>
+  )
+}
+export default Result
